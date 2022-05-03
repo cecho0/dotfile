95f93334ac3766b1baaed56a2dae57e155e503e8
cecho0 <15905922153@163.com>
2022-05-04 01:54:00 +0800

    update

---
+++ demo/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "hello world" << endl;
+    printf("d\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
