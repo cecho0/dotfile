38bc68e067a3cf3311d939e99177bc7a76969549
cecho0 <15905922153@163.com>
2022-05-04 02:08:47 +0800

    update

--- nvim/init.lua
+++
@@ -1,2 +0,0 @@
--- vim.cmd("set termguicolors")
-require("core")
--- nvim/lua/modules/remote/plugins.lua
+++
@@ -1,8 +0,0 @@
-local remote = {}
-local conf = require("modules.remote.config")
-
-remote["chipsenkbeil/distant.nvim"] = {
-    config = conf.distant,
-}
-
-return remote
